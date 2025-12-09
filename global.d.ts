2e5667c34e63999399c75236a94d404a5e032415
Carlos Turmo <zeliuk@hotmail.com>
2025-12-09 22:02:08 +0100

    Allow Tailwind elements popover and command attributes

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,17 @@
 import type React from "react";
 
-type WebComponentProps = React.HTMLAttributes<HTMLElement> & {
+type WebComponentProps = Omit<React.HTMLAttributes<HTMLElement>, "popover"> & {
   class?: string;
   anchor?: string;
-  popover?: boolean | string;
+  popover?: boolean | "" | "auto" | "manual" | "hint";
 };
+
+declare module "react" {
+  interface ButtonHTMLAttributes<T> {
+    command?: string;
+    commandfor?: string;
+  }
+}
 
 declare global {
   namespace React {
